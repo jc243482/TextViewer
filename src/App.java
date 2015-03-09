d239ea140ffe4b07127f2707bd6206af655ed35e
jc243482 <james.baker@my.jcu.edu.au>
2015-03-09 15:20:51 +1000

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jc243482 on 9/03/15.
+ */
+public class App {
+    public static void main(String[] args) {
+        String text = "this is a simple test\nOne Two Three\nHello World!";
+        for (int i = 0; i < text.length(); ++i) {
+            System.out.print(text.charAt(i));
+        }
+    }
+}
+
